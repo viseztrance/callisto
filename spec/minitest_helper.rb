464e4da62cc69b62c534d1914d2668bbc1b11ba4
Daniel Mircea <daniel@thegeek.ro>
2012-04-25 13:38:32 +0300

    Refactored spec helper into multiple files.

--- spec/minitest_helper.rb
+++ spec/minitest_helper.rb
@@ -1,53 +1,7 @@
-require "tmpdir"
-require "fileutils"
+require File.expand_path("../support/file_creator", __FILE__)
+require File.expand_path("../support/singleton", __FILE__)
 
 def reset_pool
   Callisto::Pool.settings = {}
   Callisto::Pool.reset_instance
-end
-
-class FileCreator
-
-  attr_accessor :file_name
-
-  def initialize(value)
-    self.file_name = value
-  end
-
-  def save(wait = 0)
-    sleep wait
-    FileUtils.touch path
-  end
-
-  def path
-    File.join(Dir.tmpdir, file_name)
-  end
-
-end
-
-# Reset singleton
-# http://blog.ardes.com/2006/12/11/testing-singletons-with-ruby
-
-require "singleton"
-class << Singleton
-
-  def included_with_reset(klass)
-
-    included_without_reset(klass)
-
-    class << klass
-
-      def reset_instance
-        Singleton.send :__init__, self
-        self
-      end
-
-    end
-
-  end
-
-  alias_method :included_without_reset, :included
-
-  alias_method :included, :included_with_reset
-
 end
